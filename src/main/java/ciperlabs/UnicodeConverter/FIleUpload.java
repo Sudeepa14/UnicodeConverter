0fdd34cd597290281a32f4c2c8d7041ed8ed5929
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-04-24 17:09:37 +0530

    Serving File Not tested

--- src/main/java/ciperlabs/UnicodeConverter/FIleUpload.java
+++ src/main/java/ciperlabs/UnicodeConverter/FIleUpload.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Dictionary;
 import java.util.HashMap;
 
@@ -15,9 +16,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import ConvertionEngine.Engine;
 import word.WDXToUnicode;
 
@@ -27,31 +30,71 @@ import word.WDXToUnicode;
 @Path("fileupload")
 public class FIleUpload {
 
+//	@POST
+//	@Path("/docx")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public Message upload(Message message) {
+//		
+//		System.out.println(message.getMessage());
+//		
+//		Message mes = new Message();
+//
+//        String fileName = message.getMessage();
+//        System.out.println(fileName);
+//        String[] fnameSplitted = fileName.split("\\.");
+//        String outPutFileName="";
+//        int i =0;
+//        while (i<fnameSplitted.length-1){
+//            outPutFileName+=fnameSplitted[i];
+//            i++;
+//        }
+//        outPutFileName += "-converted." + fnameSplitted[fnameSplitted.length-1];
+//        FileInputStream fileInputStream = null;
+//        try {
+//            fileInputStream = new FileInputStream(message.getMessage());
+//            try {
+//                XWPFDocument docx = new XWPFDocument(fileInputStream);
+//                WDXToUnicode docxConverter = new WDXToUnicode(docx);
+//                XWPFDocument convertedFile = docxConverter.startConversion();
+//                try {
+//
+//                    convertedFile.write(new FileOutputStream(outPutFileName));
+//                    mes.setMessage(outPutFileName);
+//                } catch (IOException e) {
+//                	e.printStackTrace();
+//                    mes.setError(true);
+//                    mes.setMessage("Error! while writing the file.");
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                mes.setError(true);
+//                mes.setMessage("Error! Incompatible document");
+//          
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//            mes.setError(true);
+//            mes.setMessage("Error! File Upload haven't completed succeffully !");
+//     
+//        }
+//		return mes;
+//	}
+	
 	@POST
-	@Path("/docx")
 	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Message upload(Message message) {
-		
-		System.out.println(message.getMessage());
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadtest(@FormDataParam("file") InputStream file, @FormDataParam("file") FormDataContentDisposition fileDetail) {
 		
 		Message mes = new Message();
+		String outPutFileName = "test.docx";
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaa");
+        
+//            FileInputStream fileInputStream = file;
 
-        String fileName = message.getMessage();
-        System.out.println(fileName);
-        String[] fnameSplitted = fileName.split("\\.");
-        String outPutFileName="";
-        int i =0;
-        while (i<fnameSplitted.length-1){
-            outPutFileName+=fnameSplitted[i];
-            i++;
-        }
-        outPutFileName += "-converted." + fnameSplitted[fnameSplitted.length-1];
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream(message.getMessage());
+//            fileInputStream = new FileInputStream(message.getMessage());
             try {
-                XWPFDocument docx = new XWPFDocument(fileInputStream);
+                XWPFDocument docx = new XWPFDocument(file);
                 WDXToUnicode docxConverter = new WDXToUnicode(docx);
                 XWPFDocument convertedFile = docxConverter.startConversion();
                 try {
@@ -69,12 +112,11 @@ public class FIleUpload {
                 mes.setMessage("Error! Incompatible document");
           
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            mes.setError(true);
-            mes.setMessage("Error! File Upload haven't completed succeffully !");
-     
-        }
-		return mes;
+        
+		return Response.ok() //200
+				.entity(mes)
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+				.allow("OPTIONS").build();
 	}
 }
