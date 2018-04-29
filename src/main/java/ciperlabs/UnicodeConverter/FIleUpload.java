0437c66d2aff5595f6ba99e24e6ae10cf55de121
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-04-29 13:09:55 +0530

    debuging

--- src/main/java/ciperlabs/UnicodeConverter/FIleUpload.java
+++ src/main/java/ciperlabs/UnicodeConverter/FIleUpload.java
@@ -38,7 +38,7 @@ import word.WDXToUnicode;
 @Path("fileupload")
 public class FIleUpload {
 //	String rootStorage = "/home/cse14/storage/";
-	String rootStorage = System.getProperty("java.io.tempdir")+"/";
+	String rootStorage = System.getProperty("java.io.tmpdir")+"/";
 	@POST
 	@Path("/docx")
 	@Produces(MediaType.APPLICATION_JSON)
